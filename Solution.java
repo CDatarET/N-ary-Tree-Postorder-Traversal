d254d3ee355c00d2afd1fce1d23db19fb5c81a9a
Chaitanya Datar <datar.chaitanya@gmail.com>
2026-05-05 22:14:34 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    private void postorder(Node root, ArrayList<Integer> list){
+        if(root == null){
+            return;
+        }
+
+        for(int i = 0; i < root.children.size(); i++){
+            postorder(root.children.get(i), list);
+        }
+
+        list.add(root.val);
+    }
+
+    public List<Integer> postorder(Node root) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        postorder(root, list);
+        return(list);
+    }
+}
